fefc6fac688a40264ec27a8b7244a361101b81a0
dkwasny <dkwasny67@gmail.com>
2016-04-30 05:13:11 -0400

    First (and probably only) pass implementation

---
+++ README.md
@@ -0,0 +1,31 @@
+Typing of the Dead: Overkill Dictionary Formatter
+=================================================
+The goal of this script is to make an attempt to format arbitrary english text such that it can be ingested into the Steam Workshop tool for The Typing of the Dead: Overkill.
+
+My personal intention is to use a few public domain books from [Project Gutenberg](https://www.gutenberg.org/wiki/Main_Page).<br/>
+This typically results in much longer phrases than what the game normally provides, which should help boost my typing skill.<br/>
+You may want to manually remove the legal text in front of and after the book contents.
+
+My limited testing has shown that TotD has a hard limit of 50 characters per line.<br/>
+Any longer and the sentence will get cut off so I prevent any lines longer thant 50 characters from being written.
+
+I added some sample input for my personal testing, but you can use it to verify the script is even working on your machine.
+
+Please feel free to let me know of any issues via email or Github issues...or whatever.
+
+How to Use
+----------
+Since TotD is not on OSX or Linux, the examples are going to be only for Windows.
+
+To just see what the output will look like, you can run a command like this in your terminal.
+        C:\path\to\python.exe totd-formatter.py -i INPUT_FILE [-m MIN_CHARACTERS_PER_LINE]
+
+To write the output to a file just add an output redirect to the end of your command like so.
+        C:\path\to\python.exe totd-formatter.py -i INPUT_FILE [-m MIN_CHARACTERS_PER_LINE] > output.txt
+
+Using the ">" operator will overwrite the contents of the output file.
+Using the ">>" operator will append to the end of the output file.
+See the [Microsoft Documentation](https://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/redirection.mspx?mfr=true) for more info on redirects.
+
+You should then be able to load the file into the workshop tool.
+Details on the tool itself can be found in [this](http://steamcommunity.com/sharedfiles/filedetails/?id=414808565) handy tutorial made by alex.marian.
---
+++ totd-formatter.py
@@ -0,0 +1,81 @@
+
+#!/usr/bin/python
+
+import re
+from argparse import ArgumentParser
+
+arg_parser = ArgumentParser(description="Format an input file for TotD Workshop consumption")
+arg_parser.add_argument(
+    "-i",
+    "--input-file",
+    help="Input File",
+    metavar="INPUT_FILE",
+    dest="input_file",
+    required=True
+)
+arg_parser.add_argument(
+    "-m",
+    "--min-characters",
+    help="Minimum number of characters per line",
+    metavar="MIN_CHARACTERS_PER_LINE",
+    dest="min_characters",
+    default=10
+)
+args = arg_parser.parse_args()
+
+with open(args.input_file, "r") as input_file:
+    input_data = input_file.read()
+
+# All "removed" sequences are actually
+# replaced with a space to preserve spacing.
+# We then normalize the spacing with a final expression.
+
+# Remove all newlines, tabs, doublequotes and underscores.
+formatted_data = re.sub("[\n\t\"_]", " ", input_data)
+
+# Remove any repeated hyphen sequences.
+# This will keep hyphenated words like "low-budget".
+formatted_data = re.sub("-{2,}", " ", formatted_data)
+
+# Remove any single quotes that are preceded by spaces.
+#
+# This should leave contractions and most possessive words alone.
+# Plural possessive nouns that end with "s" will get their
+# single quote removed. :( 
+formatted_data = re.sub("(?<=\s)'", " ", formatted_data)
+
+# Remove any single quotes that are followed by spaces.
+# See above regex for more information.
+formatted_data = re.sub("'(?=\s)", " ", formatted_data)
+
+# Remove any spacing between words and any following punctuation.
+formatted_data = re.sub("\s+([.?!])", "\\1", formatted_data)
+
+# Normalize all repeated whitespace down to a single space.
+formatted_data = re.sub("\s{2,}", " ", formatted_data)
+
+# Add a newline after question marks and exlamation points.
+formatted_data = re.sub("([?!]+)", "\\1\n", formatted_data)
+
+# Add a new line after periods when they are...
+# * followed by whitespace.
+# * NOT preceded by honorifics like (mrs. or mr.).
+# ** Honorifics are matched when they are preceded by a space so a
+#    sentence ending in "calms." doesn't trigger the "ms." honorific.
+#
+# Sentences that end with honorifics will not be matched correctly. :(
+period_pattern = re.compile(
+    "(?<!\smr)(?<!\smrs)(?<!\sms)(?<!\sdr)[.]\s+",
+    re.IGNORECASE
+)
+formatted_data = period_pattern.sub(".\n", formatted_data)
+
+min_characters = int(args.min_characters)
+output_data = ""
+for line in formatted_data.splitlines():
+    stripped_line = line.strip()
+    line_length = len(stripped_line)
+    if line_length >= min_characters and line_length <= 50:
+        output_data += stripped_line + "\n"
+
+print output_data
